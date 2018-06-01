2667e0b6fd84d38e985304330a81d0ff4a36bfc9
Maksim GAYDUK <maksim.gayduk@gmail.com>
2018-06-01 15:26:36 +0300

    parse input not formed

--- virtual_machine/sources/main.c
+++ virtual_machine/sources/main.c
@@ -6,12 +6,13 @@
 /*   By: mgayduk <maksim.gayduk@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/27 16:12:55 by mgayduk           #+#    #+#             */
-/*   Updated: 2018/05/31 20:03:21 by mgayduk          ###   ########.fr       */
+/*   Updated: 2018/06/01 14:03:09 by mgayduk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
+// make help
 int	main(int argc, char **argv)
 {
 	t_data data;
@@ -32,6 +33,5 @@ int	main(int argc, char **argv)
 			;
 		announce_the_winner(&data);
 	}
-	//system("leaks corewar");
 	return (0);
 }
